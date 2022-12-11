2247e1f153db71440302427758bedf89de8b9c73
VIAN258 <angelmorales891@gmail.com>
2022-12-11 00:22:03 +0000

    estilo listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 
+
+
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
+		
 		<body>	
 			<div className="box">
-				<div id="primero">0</div>
-				<div id="segundo">1</div>
-				<div id="tercero">2</div>
-				<div id="cuarto">3</div>
-				<div id="quinto">4</div>
-				<div id="sexto">4</div>
-				<div id="imagen">5</div>
+				<div id="primero"><h1>0</h1></div>
+				<div id="segundo"><h1>1</h1></div>
+				<div id="tercero"><h1>2</h1></div>
+				<div id="cuarto"><h1>3</h1></div>
+				<div id="quinto"><h1>4</h1></div>
+				<div id="sexto"><h1>5</h1></div>
+				<div id="imagen"><h1>6</h1></div>
 			</div>
         </body>
 	);
